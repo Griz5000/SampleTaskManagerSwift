0f58178ef6326f02ad3c05d38fb3185845d69156
Michael Grysikiewicz <mgrysikiewicz@gmail.com>
2016-03-07 08:58:40 -0500

    Initial Commit

---
+++ SampleTaskManagerSwift/Model/MSGTask.swift
@@ -0,0 +1,9 @@
+//
+//  MSGTask.swift
+//  SampleTaskManagerSwift
+//
+//  Created by Michael Grysikiewicz on 3/7/16.
+//  Copyright © 2016 Michael Grysikiewicz. All rights reserved.
+//
+
+import Foundation
