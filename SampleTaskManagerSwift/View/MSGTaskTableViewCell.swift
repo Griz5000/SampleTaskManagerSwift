8d050e3d7f6b03f1eccc9766cc3e236eedb4947e
Michael Grysikiewicz <mgrysikiewicz@gmail.com>
2016-03-09 14:32:41 -0500

    Add Storyboard layout for the project

---
+++ SampleTaskManagerSwift/View/MSGTaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MSGTaskTableViewCell.swift
+//  SampleTaskManagerSwift
+//
+//  Created by Michael Grysikiewicz on 3/9/16.
+//  Copyright © 2016 Michael Grysikiewicz. All rights reserved.
+//
+
+import UIKit
+
+class MSGTaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
